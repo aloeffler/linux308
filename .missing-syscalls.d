a0e1959222bd9fc95193dd7f42a4165eeebd039f
Andreas Loeffler <andreas@loefflertech.com>
2013-10-16 16:34:41 -0700

    Add missing files

---
+++ .missing-syscalls.d
@@ -0,0 +1,6 @@
+-:  include/generated/autoconf.h \
+ /home/andreas/kernel/linux-3.0.8/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ /home/andreas/kernel/linux-3.0.8/arch/arm/include/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/andreas/kernel/linux-3.0.8/arch/arm/include/asm/unistd.h
